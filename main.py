816c8b3fd31b4dc08d07595f6bbcd604045335d3
Kreoniz <Kreoniz@mail.ru>
2022-04-12 18:59:29 +0600

    added two articles + slight fixes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, url_for, redirect
 import os
 
-
 app = Flask(__name__,
             static_url_path='', 
             static_folder='static',
@@ -24,19 +23,30 @@ def roadmap():
     params['css_dest'] = url_for('static', filename='css/roadmap.css')
     return render_template('roadmap.html', **params)
 
-# <----------- All roadmap-associated files ----------->
-@app.route('/roadmap/a/whats_immersion')
+# * <--------------------------- All roadmap-associated files --------------------------->
+
+@app.route('/roadmap/stage-0/a/whats_immersion')
 def whats_immersion():
     params = {}
     params['css_dest'] = url_for('static', filename='css/article.css')
     params['crane_logo'] = url_for('static', filename='img/icons/symbol-contrast.svg')
     return render_template('stage-0/a/whats_immersion.html', **params)
 
+@app.route('/roadmap/stage-0/a/activities_explained')
+def activities_explained():
+    params = {}
+    params['css_dest'] = url_for('static', filename='css/article.css')
+    params['crane_logo'] = url_for('static', filename='img/icons/symbol-contrast.svg')
+    return render_template('stage-0/a/activities_explained.html', **params)
 
+@app.route('/roadmap/stage-0/a/whats_my_level')
+def whats_my_level():
+    params = {}
+    params['css_dest'] = url_for('static', filename='css/article.css')
+    params['crane_logo'] = url_for('static', filename='img/icons/symbol-contrast.svg')
+    return render_template('stage-0/a/whats_my_level.html', **params)
 
-
-
-# <----------- All roadmap-associated files ----------->
+# * <--------------------------- All roadmap-associated files --------------------------->
 
 @app.route('/about')
 def about():
